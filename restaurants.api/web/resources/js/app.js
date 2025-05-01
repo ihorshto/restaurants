313ac6b1c691b82afaddc3387e342132b404f20f
Ihor Shtohryn <ihor@ecaste.com>
2025-05-01 15:40:25 +0200

    Install dropzone and lodash

--- restaurants.api/web/resources/js/app.js
+++ restaurants.api/web/resources/js/app.js
@@ -1,8 +1,12 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
-import 'preline'
+import 'preline';
+import _ from 'lodash';
+import Dropzone from 'dropzone';
 
 window.Alpine = Alpine;
+window._ = _;
+window.Dropzone = Dropzone;
 
 Alpine.start();
