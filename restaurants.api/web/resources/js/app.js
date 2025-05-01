f902d74ed9138a8d8ffa9341198e8c4ec68a7a88
Ihor Shtohryn <ihor@ecaste.com>
2025-05-01 23:08:59 +0200

    Import HSFileUpload

--- restaurants.api/web/resources/js/app.js
+++ restaurants.api/web/resources/js/app.js
@@ -5,9 +5,11 @@ import Alpine from 'alpinejs';
 import 'preline';
 import _ from 'lodash';
 import Dropzone from 'dropzone';
+import HSFileUpload from 'preline/dist/file-upload.js';
 
 window.Alpine = Alpine;
 window._ = _;
 window.Dropzone = Dropzone;
+window.HSFileUpload = HSFileUpload;
 
 Alpine.start();
