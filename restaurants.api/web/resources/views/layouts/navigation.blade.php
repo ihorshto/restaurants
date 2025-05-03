04243c6cb1b6ecf6f2bb6b6c997c227845d27fb9
Ihor Shtohryn <ihor@ecaste.com>
2025-05-03 10:46:03 +0200

    Restaurants tab in the navbar

--- restaurants.api/web/resources/views/layouts/navigation.blade.php
+++ restaurants.api/web/resources/views/layouts/navigation.blade.php
@@ -15,6 +15,10 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+
+                    <x-nav-link :href="route('restaurants.index')" :active="request()->route()->named('restaurants.*')">
+                        {{ __('Restaurants') }}
+                    </x-nav-link>
                 </div>
             </div>
 
