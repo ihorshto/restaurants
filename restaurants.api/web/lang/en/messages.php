2f99e4af5659c9f9319ccbd11f386ed27688a03d
Ihor Shtohryn <ihor@ecaste.com>
2025-05-03 10:30:33 +0200

    Add language package

---
+++ restaurants.api/web/lang/en/messages.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'create' => 'Create',
+    'restaurants' => [
+        'create_restaurant' => 'Create Restaurant',
+        'edit_restaurant' => 'Edit Restaurant',
+        'delete_restaurant' => 'Delete Restaurant',
+        'restaurant_name' => 'Restaurant Name',
+        'restaurant_description' => 'Restaurant Description',
+    ],
+    'name' => 'Name',
+    'description' => 'Description',
+    'file' => [
+        'no_chosen' => 'No file chosen',
+        'choose' => 'Choose a file',
+        'exeeds_max_size' => 'The file exceeds the maximum allowed size of :max_size MB',
+        'invalid_type' => 'Invalid file type. Only :types files are allowed',
+        'upload_failed' => 'File upload failed',
+    ],
+    'image' => 'Image',
+    'menu' => 'Menu',
+    'longitude' => 'Longitude',
+    'latitude' => 'Latitude',
+];
---
+++ restaurants.api/web/lang/fr/messages.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    'create' => 'Créer',
+    'restaurants' => [
+        'create_restaurant' => 'Créer un restaurant',
+        'edit_restaurant' => 'Modifier le restaurant',
+        'delete_restaurant' => 'Supprimer le restaurant',
+        'restaurant_name' => 'Nom du restaurant',
+        'restaurant_description' => 'Description du restaurant',
+        'restaurant_longitude' => 'Longitude du restaurant',
+        'restaurant_latitude' => 'Latitude du restaurant',
+    ],
+    'name' => 'Nom',
+    'description' => 'Description',
+    'file' => [
+        'no_chosen' => 'Aucun fichier choisi',
+        'choose' => 'Choisir un fichier',
+        'exeeds_max_size' => 'Le fichier dépasse la taille maximale autorisée de :max_size MB',
+        'invalid_type' => 'Type de fichier invalide. Seuls les fichiers :types sont autorisés',
+        'upload_failed' => 'Échec du téléchargement du fichier',
+    ],
+    'image' => 'Image',
+    'menu' => 'Menu',
+    'longitude' => 'Longitude',
+    'latitude' => 'Latitude',
+];
