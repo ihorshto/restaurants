2e8c3e98ee7db1c8407317e952d419ccfeaef067
Ihor Shtohryn <ihor@ecaste.com>
2025-05-04 16:59:15 +0200

    Add create button

--- restaurants.api/web/resources/views/restaurants/index.blade.php
+++ restaurants.api/web/resources/views/restaurants/index.blade.php
@@ -1,18 +1,19 @@
 <x-app-layout>
-        <a href="{{route('restaurants.create')}}" class="button-default bg-gray-500 hover:bg-gray-600 text-white">{{__('messages.create')}}</a>
+    <a href="{{route('restaurants.create')}}" class="button-default bg-gray-500 hover:bg-gray-600 text-white">{{__('messages.create')}}</a>
 
-        <div class="grid sm:grid-cols-2 lg:grid-cols-3 gap-6">
-            @foreach($restaurants as $restaurant)
-                <x-restaurant-card
-                    name="{{$restaurant->name}}"
-                    description="{{$restaurant->description}}"
-                    imagePath="{{$restaurant->image_path}}"
-                    menuPath="{{$restaurant->menu_path}}"
-                    longitude="{{$restaurant->longitude}}"
-                    latitude="{{$restaurant->latitude}}"
-                    :tags="$restaurant->tags"/>
-            @endforeach
-        </div>
+    <div class="grid sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        @foreach($restaurants as $restaurant)
+            <x-restaurant-card
+                :id="$restaurant->id"
+                name="{{$restaurant->name}}"
+                description="{{$restaurant->description}}"
+                imagePath="{{$restaurant->image_path}}"
+                menuPath="{{$restaurant->menu_path}}"
+                longitude="{{$restaurant->longitude}}"
+                latitude="{{$restaurant->latitude}}"
+                :tags="$restaurant->tags"/>
+        @endforeach
+    </div>
 
     <script>
         function openMap(latitude, longitude) {
