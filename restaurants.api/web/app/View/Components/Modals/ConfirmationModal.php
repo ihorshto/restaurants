01fd53c62aaf643d1895b64b3cfe54a4d7fdf93a
Ihor Shtohryn <ihor@ecaste.com>
2025-05-04 15:13:24 +0200

    Create confirmation modal

---
+++ restaurants.api/web/app/View/Components/Modals/ConfirmationModal.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\View\Components\Modals;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class ConfirmationModal extends Component
+{
+    public $id;
+    public $title;
+    public $subtitle;
+    public $confirmButtonType;
+    public function __construct($id, $title, $subtitle, $confirmButtonType)
+    {
+        $this->id = $id;
+        $this->title = $title;
+        $this->subtitle = $subtitle;
+        $this->confirmButtonType = $confirmButtonType;
+    }
+
+    public function render(): View|Closure|string
+    {
+        return view('components.modals.confirmation-modal');
+    }
+}
